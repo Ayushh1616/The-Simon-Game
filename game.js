92ce0036fdc76b6e678a12ffb8e8c20ada488cd9
Ayushh1616 <143823835+Ayushh1616@users.noreply.github.com>
2023-09-15 18:32:19 +0530

    game.js

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
-var buttonColors=["red","blue","green","yellow"];
-var gamePattern=[];
+let buttonColors=["red","blue","green","yellow"];
+let gamePattern=[];
 
-var userClickedPattern=[];
+let userClickedPattern=[];
 
-var started=false;
+let started=false;
 
-var level=0;
+let level=0;
 
 $(document).keypress(function(){
     if(!started){
@@ -17,7 +17,7 @@ $(document).keypress(function(){
 
 
 $(".btn").click(function(){
-    var userChosenColour=$(this).attr("id");   //NOT UNDERSTOOD--.attr("id") is used to retrieve the "id" attribute of the clicked element. The attr() function is a jQuery method for getting or setting attributes on HTML elements.
+    let userChosenColour=$(this).attr("id");   //NOT UNDERSTOOD--.attr("id") is used to retrieve the "id" attribute of the clicked element. The attr() function is a jQuery method for getting or setting attributes on HTML elements.
     userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
     animatePress(userChosenColour);
@@ -67,8 +67,8 @@ function nextSequence() {
     $("#level-title").text("Level " + level);
 
 
-    var randomNumber=Math.floor(Math.random()*4);
-    var randomChosenColour=buttonColors[randomNumber];
+    let randomNumber=Math.floor(Math.random()*4);
+    let randomChosenColour=buttonColors[randomNumber];
     gamePattern.push(randomChosenColour);
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
@@ -78,7 +78,7 @@ function nextSequence() {
 }
 function playSound(name) {
 
-    var audio = new Audio("sounds/" + name + ".mp3");
+    let audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
     
   }
